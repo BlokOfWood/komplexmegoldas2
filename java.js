ae796ef36caa854b4b89e80fe7400d44b0e7f8f4
StrikerElite <strikerelite@outlook.hu>
2020-11-19 13:50:26 +0100

    Quiz kidolgozás

--- java.js
+++ java.js
@@ -21,20 +21,26 @@ function hashString(string)
       h = (h << 5) - h + string.charCodeAt(i++) | 0;
   return h;
 }
-var quizElement = document.getElementById("Quiz");
+var quizElement;
 
 Quiz = [
     {
         Question: "Test",
         Answers: ["Answer1", "Answer2", "Answer3", "Answer4"],
-        AnswerHash: ""
+        AnswerHash: "817254420"
     }
 ]
+function init()
+{
+  quizElement = document.getElementById("Quiz");;
+}
 
 function addQuizElement (questionIndex) {
     quizElement.innerHTML += 
         "<h2>" + Quiz[questionIndex].Question + "</h2>\n";
     Quiz[questionIndex].Answers.forEach(element => {
-        quizElement.innerHTML += "<li>"+element+"</li>\n"
+        quizElement.innerHTML += 
+          '<input type="radio" id="AnswerInput" name="'+Quiz[questionIndex].Question+'+" value="+'+element+'">'+
+          '<label for="'+element+'">'+element+'</label><br>'
     });
 }
